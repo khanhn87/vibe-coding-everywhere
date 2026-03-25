05240b06c9f426001e849ccd5221acbf71d5343a
khanhn <khanhn@hrcloud.com>
2026-03-25 21:10:50 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+BRANCH="${BRANCH:-main}"
+SOURCE_DIR="${SOURCE_DIR:-$(pwd)}"
+
+cd "$SOURCE_DIR"
+git fetch --prune origin "$BRANCH"
+git reset --hard "origin/$BRANCH"
+git clean -fd
+  
 git pull origin
 	
